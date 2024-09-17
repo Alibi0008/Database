ef58942fae65405a42f1b96655ba5951b9c89189
Alibi0008 <156880160+Alibi0008@users.noreply.github.com>
2024-09-17 14:59:03 +0500

    lab1

---
+++ lab1.sql
@@ -0,0 +1,27 @@
+create  database lab1;
+
+create table users (
+    id serial,
+    firstname varchar(50),
+    lastname varchar(50)
+);
+
+drop table users;
+
+alter table users add column isadmin int;
+
+alter table users alter column isadmin type boolean using isadmin::boolean;
+
+alter table users alter column isadmin set default FALSE;
+
+alter table users add primary key (id);
+
+create table tasks (
+    id serial,
+    name varchar(50),
+    user_id int
+);
+
+drop table tasks;
+
+drop database lab1;
